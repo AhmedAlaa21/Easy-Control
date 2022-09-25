e846c400518b3f90d73e16a57856639323f70aaa
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2022-09-25 15:43:36 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, Navbar, Cart } from "./components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const { cart } = await commerce.cart.add(productId, quantity);
+    // add the item to the cart after being added from user.
+    setCart(cart);
+  };
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+    setCart(cart);
+  };
+  
+  //remove item from cart
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  };
+  // empty the cart
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  return (
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Products products={products} handleAddToCart={handleAddToCart} />
+            }
+          />
+
+          <Route
+            exact
+            path="/cart"
+            element={
+              <Cart
+                cart={cart}
+                handleUpdateCartQty={handleUpdateCartQty}
+                handleRemoveFromCart={handleRemoveFromCart}
+                handleEmptyCart={handleEmptyCart}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Typography, Button, Grid, Container } from "@material-ui/core";
+import useStyles from "./styles";
+import { Audio } from "react-loader-spinner";
+import CartItem from "./CartItem/CartItem";
+import { Link } from "react-router-dom";
+
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
+  const classes = useStyles();
+  //will display cart or empty cart according to this.
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">
+      You have no items in your shopping cart,
+      <Link to="/" className={classes.link}>
+        start adding some!
+      </Link>
+    </Typography>
+  );
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem
+              item={item}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">
+          Subtotal: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            onClick={handleEmptyCart}
+            size="large"
+            color="secondary"
+            type="button"
+            variant="contained"
+          >
+            Empty Cart
+          </Button>
+          <Button
+            className={classes.checkoutButton}
+            size="large"
+            color="primary"
+            type="button"
+            variant="contained"
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+
+  if (!cart.line_items)
+    return (
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperClass={classes.audio}
+      />
+    );
+
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        Your Shopping Cart
+      </Typography>
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
